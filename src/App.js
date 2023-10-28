6906f9454f9668108bc47e5bd0288aed2ecf95f7
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2023-10-28 17:31:17 -0230

    now uses app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import { Outlet, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Order from "./pages/Order";
+
+const linkStyle = {
+  textDecoration: 'none',
+  color: 'white',
+  text: '30pt',
+  padding: '30px',
+  fontSize: '30pt'
+};
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+    <header className='header'>
+    <nav>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/menu" style={linkStyle}>Menu</Link>
+      <Link to="/order" style={linkStyle}>Order Online</Link>
+    </nav>
+    </header>
+    <Outlet />
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+          <Route index element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="order" element={<Order />} />
+      </Routes>
+      </>
   );
-}
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import Order from "./pages/Order";
-
-export default function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="menu" element={<Menu />} />
-            <Route path="order" element={<Order />} />
-            
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+import { BrowserRouter} from "react-router-dom";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <BrowserRouter>
     <App />
-  
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
