4d141f2e0d21ae8b0a0d64afe2ae99e60d3a5806
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2023-11-03 11:13:55 -0230

    new home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,7 @@
+import MyImageComponent from "../components/ImageComponent";
+import MyImageComponent2 from "../components/ImageComponent2";
+import MyImageComponent3 from "../components/ImageComponent3";
+
 import "./Home.css"
 
 const Home = () => {
@@ -12,6 +16,35 @@ const Home = () => {
           <div className="stars">&#9733;</div>
           <div className="stars">&#9733;</div>
           <div className="stars">&#9733;</div>
+        </div>
+        <div className="homeLocationBox">
+          <div>
+            <MyImageComponent/>
+          </div>
+          <div>
+            <p id="locationHours">We're located on 405 Cerulean Road<br/><br/>
+            Hours of Operation:<br/>
+            Mon-Fri: 6am - 9pm<br/>
+            Sat: 8am - 10pm<br/>
+            Sun: 8am - 6pm
+            </p>
+          </div>
+          <div>
+            <MyImageComponent2/>
+          </div>
+        </div>
+        <div className="homeSummaryBox">
+          <div>
+            <MyImageComponent3/>
+          </div>
+          <div>
+            <p id="summaryText">Gary Blue's diner was opened in 1972 with the goal of providing<br/>
+            quality food and a cozy atmosphere to our patrons. Lorem Ipsum<br/>
+            dolor sit amet, consectetur adipiscing elit. Nunc pretium<br/>
+            ultrices dui. Phasellus lorem nisl convallis sit amet justo vitae<br/>
+            dignissim vulputate.
+            </p>
+          </div>
         </div>
       </div>
     </>
